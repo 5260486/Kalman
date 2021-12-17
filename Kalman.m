04a5c8ba20d10a91c47d88d461ecd90c7c538f0e
卿旸 <45734103+5260486@users.noreply.github.com>
2021-12-17 19:45:54 +0800

    Update Kalman.m
    改正温度修正系数

--- Kalman.m
+++ Kalman.m
@@ -1,74 +1,141 @@
 %% 卡尔曼滤波
+
+%%获得数据
 mpc=datapretreat;
 data=mpc.timeload;
 s=size(data);
-P=[];
+
 %% 参数设置
 N =s(2) ;     % 设置数据长度为N
 t = (1:N);   % 生成时间轴
-a = 1;       % 状态转移方程，为一维数据时，a的值是1
+fai = 1;     % 状态转移方程，本例中是连续多天同一时刻的温度，可看作为缓变状态，可以认为是1
 b = 0;       % 控制输入
-c = 1;       % 当sigma2_是一维数据时，c: 观测方程=1
-x = 5;       % 设置初值 初始的均值和方差 
+X = 0;       % 设置初值
+P=[];
 for i=1:s(1)
     A=data(i,:);
     B= cov(A); % 初始系统协方差
-    P(end+1)=B;
+    P(end+1)=B; %初始协方差阵
 end
+
 V = 50;      % 设置生成的信号的噪声标准差
 Q = 1;       % 设置状态转移方差Q和观测方差R
-R = 0.5;     % 对象测量噪声的协方差  
-
+R = 0.05;     % 对象测量噪声的协方差  
 
 %% 初始化
-real_signal = zeros(1,N); % 真实信号
-z = zeros(1,N);           % 观测数据
-x_filter = zeros(1,N);    % 存储卡尔曼估计的结果，缓存起来用于画图形
-K = zeros(1,N);           % 存储卡尔曼预测中的增益k，缓存起来用于画图形
-y=[];
+% y = zeros(1,N);           % 观测数据
+x_filter = zeros(24,1);    % 存储卡尔曼估计的结果，缓存起来用于画图形
+x_filter2 = zeros(24,1); 
+K = zeros(24,3);           % 存储卡尔曼预测中的增益k，缓存起来用于画图形
 
-% 初始化真实数据和观测数据
- for j=1:24
-    for i=1:N
-       %真实数据
-       real_signal(i) = mpc.timeload(j,i); 
-       %观测数据，基本负荷+前一天同时刻负荷+该时刻气温+误差
-       z(i) = real_signal(i) + normrnd(0, V);
+%% 初始化真实数据和观测数据
+% Lk=Hnk*Lnk+Hpk*Lpk+Htk*Ltk+vk; %第t天k时刻负荷=该时刻基本负荷Lnk+前一天同时刻负荷Lpk+该时刻气温Ltk+误差vk
+Lnksum=zeros(24,1);
+Lnk=zeros(24,1);
+Ltksum=zeros(24,1);
+Ltk=zeros(24,1);
+Lk=mpc.timeload(:,2:N);
+for j=1:24
+    for i=1:N-1
+        Lnksum(j)=mpc.timeload(j,i)+Lnksum(j);  
+        Ltksum(j)=mpc.tem(j,i)+Ltksum(j);
     end
+        Lnk=Lnksum/(N-1);   
+        Ltk=Ltksum/(N-1);
+end
 
-    %% 卡尔曼滤波
-    for i=1:N
+Hnk=1; %第k时刻负荷值的参数矩阵，可看作不变
+bsum=zeros(3,1);
+
+for i=1:N-1
+%     L=abs(Lk-Hnk*Lnk);
+    L=Lk-Hnk*Lnk;
+    L=L(:,i);
+    Lpk=mpc.timeload(:,i);
+    Lkx=[ones(24,1),Lpk,Ltk];
+    [br,bint,r,rint,s]=regress(L,Lkx); %线性回归得到Hpk，Htk
+    vk=br(1);
+    Hpk=br(2);
+    Htk=br(3);
+    H=[Hnk,Hpk,Htk]; % 观测矩阵
+    Hk(i,:)=H;
+    X=[Lnk,Lpk,Ltk];
+    y=X*H'+vk;
+    if i==1
+        xk.x1=X;
+    end
+    if i==2
+        xk.x2=X;
+    end
+    if i==3 
+        xk.x3=X; %得到真实数据
+    end  
+    Y(:,i)=y;    %得到观测数据
+
+end  
+
+%% 卡尔曼滤波
+T=mpc.tem;
+u=[30 30 -10 30 30 100 30 -10 -10 0 -100 -100 -100 -100 30 100 100 30 30 30 30 -100 -100 -100 -100]; %试验所得修正系数
+for j=1:24  %每个小时
+    y=Y(j,:);%单个小时前三天的观测值
+    for i=1:N-1  %前三天
+        if i==1
+            X=xk.x1;
+        end
+        if i==2
+            X=xk.x2;
+        end
+        if i==3 
+            X=xk.x3; %真实数据
+        end  
+        x=X(j,:);
+        H=Hk(i,:);
+        
         % 预测步
-        x_ = a*x + b;            %预测当前状态
-        P_(j) = a*P(j)*a'+Q;
+        x_ = fai*x + b;            %预测当前状态
+        P_(j) = fai*P(j)*fai'+Q;
 
         % 更新步
-        k = P_(j)*c'/(c*P_(j)*c'+R);
-        x = x_ + k*(z(i) - c*x_);
-        P(j) = (1-k*c)*P_(j); %更新协方差
+        k = P_(j)*H'/(H*P_(j)*H'+R);
+        x = x_ + k'*(y(i) - x_*H');
+        P(j) = (1-H*k)*P_(j); %更新协方差
 
         % 存储滤波结果
-        x_filter(i) = x;
-        K(i) = k;
-        
-        if i==N
-            y(end+1)=x; %估计值
+        if i==N-1
+            x_filter(j)=x*H';
+            x_filter2(j)=x*H'+u(j)*(T(j,i)-Ltk(j));%对当天气温进行修正
+            K(j,:) = k;
         end
     end
- end
+end
 
+%% 画出预测值与实际值的对比图
 y1=[];
 for i=(24*(N-1)+1):24*N
-    y1(end+1)=mpc.b1(i);%真实值
+    y1(end+1)=mpc.b1(i);%第四天的真实值
 end
-date=0:23;
-plot(date,y,date,y1);grid on;
-legend('kalman','real');
+hour=0:23;
 
-%% 展示
-% %画出卡尔曼增益k 可以看到系统很快会达到稳态，k会很快收敛成为一个常数，此时卡尔曼滤波也就相当于低通滤波了
-% plot(t, K);legend('K');
-% % 画出波形， 真实值 - 观测值 - 卡尔曼估计值
-% figure(2)
-% plot(t, real_signal, 'r', t, z, 'g', t, x_filter, 'b')
-% legend('real','measure','filter');
+% plot(hour,x_filter,'b-o',hour,y1,'k-+');grid on;
+% legend('Kalman1','Real');
+% xlabel('t/h');ylabel('Load/W');
+
+% plot(hour,x_filter2,'r-o',hour,y1,'k-+');grid on;
+% legend('Kalman2','Real');
+% xlabel('t/h');ylabel('Load/W');
+
+% plot(hour,x_filter,'b-*',hour,x_filter2,'r-o',hour,y1,'k-+');grid on;
+% legend('Kalman1','Kalman2','Real');
+% xlabel('t/h');ylabel('Load/W');
+
+%% 绝对误差计算
+d1=[];
+d2=[];
+for i=1:24
+    d1(end+1)=100*abs(x_filter(i)-y1(i))/y1(i);
+    d2(end+1)=100*abs(x_filter2(i)-y1(i))/y1(i);
+end
+% bar(hour,d1);
+bar(hour,d2);
+xlabel('t/h');ylabel('误差%');
